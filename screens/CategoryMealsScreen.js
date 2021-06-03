43425fa4e8c463ed922dd4ccf1a36208ffaba420
Shahriar Shubho <shubha.shahriar@gamil.com>
2021-06-03 19:39:03 +0600

    project starting

---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text } from 'react-native'
+
+const CategoryMealsScreen = props => {
+    return (
+        <View style={styles.screen}>
+          <Text>Category Meals Screen component</Text>
+        </View>
+      );
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
+
+export default CategoryMealsScreen
